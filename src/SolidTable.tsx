22cb4fd75c2c623de30c352ae2f8a3c6b4b2d6c5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-24 00:56:40 -0600

    Add the SolidTable component

--- src/SolidTable.tsx
+++ src/SolidTable.tsx
@@ -1,3 +1,5 @@
+import { createState } from "solid-js"
+
 // util types
 type AnyObject = Record<string, any>
 type Renderable = any
@@ -22,3 +24,77 @@ export type Props = {
 
 export type State = { sort: SortInfo | null }
 
+export function SolidTable(props: Props) {
+  const [state, setState] = createState<State>({ sort: null })
+
+  function getSortInfo(): SortInfo {
+    return state.sort || props.initialSort || []
+  }
+
+  function generateSortCallback(column: string) {
+    return (e: MouseEvent) => {
+      const sortInfo = getSortInfo()
+      const append = e.shiftKey
+      clickHandler(sortInfo, column, append)
+      setState({ sort: sortInfo })
+    }
+  }
+
+  const {
+    rows: givenRows,
+    columns,
+    className = "",
+    rowKey,
+    sort,
+    renderHeaderColumn = defaultHeaderRenderer,
+    renderBodyColumn = defaultBodyRenderer,
+  } = props
+
+  let rows = givenRows
+  const sortInfo = getSortInfo()
+
+  if (sortInfo.length) {
+    rows = sort(sortInfo, rows)
+  }
+
+  return (
+    <table className={`solid-table ${className}`} style={props.style}>
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th
+              key={column.key}
+              className={column.sortable ? "sortable" : undefined}
+              onClick={column.sortable ? generateSortCallback(column.key) : undefined}
+            >
+              {renderHeaderColumn(column)} {column.sortable && renderHeaderIcon(getSortInfo(), column.key)}
+            </th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {rows.map(function (row) {
+          const key = rowKey(row)
+          return (
+            <tr key={key}>
+              {columns.map(function (column) {
+                const givenOnClick = column.onClick
+                const onClick =
+                  givenOnClick &&
+                  function (e: MouseEvent) {
+                    givenOnClick(e, row)
+                  }
+
+                return (
+                  <td onClick={onClick} key={`${key}.${column.key}`}>
+                    {renderBodyColumn(row, column.key)}
+                  </td>
+                )
+              })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
+  )
+}
