24fbd095a5a597731787557010c22aaa9629e6af
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-25 04:49:32 -0600

    Stronger typing my making key and values generic

--- README.md
+++ README.md
@@ -76,23 +76,22 @@ function MyTable() {
 ## API
 
 ```ts
-
 <SimpleTable
   // row and column
-  rows: Array<Row>
-  columns: Array<Column>
+  rows: Array<Row<K, V>>
+  columns: Array<Column<K, V>>
 
   // renderers
   headerRenderer(column: Column): string | Renderable
-  bodyRenderer(row: Row, columnKey: string): string | Renderable
+  bodyRenderer(row: Row, columnKey: K): string | Renderable
 
   // styles
   style?: AnyObject
   className?: string
 
   // sort options
-  initialSort?: SortInfo
-  sort(sortInfo: SortInfo, rows: Array<Row>): Array<Row>
+  initialSort?: SortInfo<K>
+  sort(sortInfo: SortInfo<K>, rows: Array<Row>): Array<Row>
 
   /** a function that takes row and returns string unique key for that row */
   rowKey(row: Row): string
@@ -104,15 +103,18 @@ In which:
 
 ```ts
 // util types
-type AnyObject = Record<string, any>
+export type AnyObject = Record<string, any>
 type Renderable = any
 
 // row and column types
-type Row = AnyObject
-type Column = { key: string; label: string; sortable?: boolean; onClick?(e: MouseEvent, row: Row): void }
+export type Key = string
+export type Row<K extends Key = string, V = any> = Record<K, V>
+
+export type Column<K extends Key = string, V = any> = { key: K; label: string; sortable?: boolean; onClick?(e: MouseEvent, row: Row<K, V>): void }
 
 // sort info
-type SortInfo = Array<{ columnKey: string; type: "asc" | "desc" }>
+export type SortInfo<K = Key> = Array<{ columnKey: K; type: "asc" | "desc" }>;
+
 ```
 
 ## License
--- src/SimpleTable.tsx
+++ src/SimpleTable.tsx
@@ -6,29 +6,31 @@ export type AnyObject = Record<string, any>
 type Renderable = any
 
 // row and column types
-export type Row = AnyObject
-export type Column = { key: string; label: string; sortable?: boolean; onClick?(e: MouseEvent, row: Row): void }
+export type Key = string
+export type Row<K extends Key = string, V = any> = Record<K, V>
+
+export type Column<K extends Key = string, V = any> = { key: K; label: string; sortable?: boolean; onClick?(e: MouseEvent, row: Row<K, V>): void }
 
 // sort info
-export type SortInfo = Array<{ columnKey: string; type: "asc" | "desc" }>
+export type SortInfo<K = Key> = Array<{ columnKey: K; type: "asc" | "desc" }>
 
 // Props
-export type Props = {
+export type Props<K extends Key = string, V = any> = {
   // row and column
-  rows: Array<Row>
-  columns: Array<Column>
+  rows: Array<Row<K, V>>
+  columns: Array<Column<K, V>>
 
   // renderers
   headerRenderer(column: Column): string | Renderable
-  bodyRenderer(row: Row, columnKey: string): string | Renderable
+  bodyRenderer(row: Row, columnKey: K): string | Renderable
 
   // styles
   style?: AnyObject
   className?: string
 
   // sort options
-  initialSort?: SortInfo
-  sort(sortInfo: SortInfo, rows: Array<Row>): Array<Row>
+  initialSort?: SortInfo<K>
+  sort(sortInfo: SortInfo<K>, rows: Array<Row>): Array<Row>
 
   /** a function that takes row and returns string unique key for that row */
   rowKey(row: Row): string
@@ -124,7 +126,7 @@ function defaultHeaderRenderer(item: any) {
   return item
 }
 
-function defaultBodyRenderer(row: AnyObject, columnKey: string) {
+function defaultBodyRenderer(row: Row, columnKey: Key) {
   const value = row[columnKey]
   if (typeof value !== "string") {
     throw new Error("Non-predictable rows fed to solid-simple-table without bodyRenderer prop")
@@ -132,7 +134,7 @@ function defaultBodyRenderer(row: AnyObject, columnKey: string) {
   return value
 }
 
-function findSortItemByKey(sortInfo: SortInfo, columnKey: string): number {
+function findSortItemByKey(sortInfo: SortInfo, columnKey: Key): number {
   if (Array.isArray(sortInfo)) {
     for (let i = 0, length = sortInfo.length; i < length; ++i) {
       if (sortInfo[i].columnKey === columnKey) {
@@ -153,14 +155,14 @@ function renderHeaderIcon(sortInfo: SortInfo, column: string) {
   return <span className="sort-icon">{icon}</span>
 }
 
-function clickHandler(sortInfo: SortInfo, columnKey: string, append: boolean) {
+function clickHandler(sortInfo: SortInfo, columnKey: Key, append: boolean) {
   const index = findSortItemByKey(sortInfo, columnKey)
   if (index < 0) {
-    const value: { columnKey: string; type: "asc" | "desc" } = { columnKey, type: "asc" }
+    const value: { columnKey: Key; type: "asc" | "desc" } = { columnKey, type: "asc" }
     sortInfo = append ? sortInfo : []
     sortInfo.push(value)
   } else {
-    const value: { columnKey: string; type: "asc" | "desc" | null } = sortInfo[index]
+    const value: { columnKey: Key; type: "asc" | "desc" | null } = sortInfo[index]
     value.type = value.type === "asc" ? "desc" : null
     if (!append) {
       sortInfo = (value.type !== null ? [value] : []) as SortInfo
