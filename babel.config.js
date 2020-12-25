a005d4bd9a8a24f36ab71f711532b56a78dfd670
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-24 18:58:13 -0600

    Fix jest config

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+const plugins = process.env.BABAEL_CJS ? ["@babel/plugin-transform-modules-commonjs"] : []
+
+module.exports = {
+  "presets": [
+    "babel-preset-solid",
+    "@babel/preset-typescript"
+  ],
+  plugins
+}
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
-exports = {
+module.exports = {
   transform: {
     "^.+\\.[tj]sx?$": "babel-jest",
   },
-  preset: "babel-jest",
   setupFilesAfterEnv: ["./jest.setup.ts"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/src/__mocks__/fileMock.js",
