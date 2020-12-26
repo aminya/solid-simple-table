3dc17694a2e0671091858bc48fa51f3d02658847
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-26 02:20:15 -0600

    chore: move types to a seprate file

--- src/SimpleTable.tsx
+++ src/SimpleTable.tsx
@@ -1,47 +1,8 @@
 import { createState } from "solid-js"
 import "./SimpleTable.css"
+import { Props, State, SortInfo, Row, Key } from "./SimpleTable.types"
 
-// util types
-export type AnyObject = Record<string, any>
-type Renderable = any
-
-// row and column types
-export type Key = string
-export type Row<K extends Key = string, V = any> = Record<K, V>
-
-export type Column<K extends Key = string, V = any> = {
-  key: K
-  label: string
-  sortable?: boolean
-  onClick?(e: MouseEvent, row: Row<K, V>): void
-}
-
-// sort info
-export type SortInfo<K = Key> = Array<{ columnKey: K; type: "asc" | "desc" }>
-
-// Props
-export type Props<K extends Key = string, V = any> = {
-  // row and column
-  rows: Array<Row<K, V>>
-  columns: Array<Column<K, V>>
-
-  // renderers
-  headerRenderer(column: Column): string | Renderable
-  bodyRenderer(row: Row, columnKey: K): string | Renderable
-
-  // styles
-  style?: AnyObject
-  className?: string
-
-  // sort options
-  initialSort?: SortInfo<K>
-  sort(sortInfo: SortInfo<K>, rows: Array<Row>): Array<Row>
-
-  /** a function that takes row and returns string unique key for that row */
-  rowKey(row: Row): string
-}
-
-export type State = { sort: SortInfo | null }
+export * from "./SimpleTable.types"
 
 export function SimpleTable(props: Props) {
   const [state, setState] = createState<State>({ sort: null })
---
+++ src/SimpleTable.types.ts
@@ -0,0 +1,41 @@
+// util types
+export type AnyObject = Record<string, any>
+type Renderable = any
+
+// row and column types
+export type Key = string
+export type Row<K extends Key = string, V = any> = Record<K, V>
+
+export type Column<K extends Key = string, V = any> = {
+  key: K
+  label: string
+  sortable?: boolean
+  onClick?(e: MouseEvent, row: Row<K, V>): void
+}
+
+// sort info
+export type SortInfo<K = Key> = Array<{ columnKey: K; type: "asc" | "desc" }>
+
+// Props
+export type Props<K extends Key = string, V = any> = {
+  // row and column
+  rows: Array<Row<K, V>>
+  columns: Array<Column<K, V>>
+
+  // renderers
+  headerRenderer(column: Column): string | Renderable
+  bodyRenderer(row: Row, columnKey: K): string | Renderable
+
+  // styles
+  style?: AnyObject
+  className?: string
+
+  // sort options
+  initialSort?: SortInfo<K>
+  sort(sortInfo: SortInfo<K>, rows: Array<Row>): Array<Row>
+
+  /** a function that takes row and returns string unique key for that row */
+  rowKey(row: Row): string
+}
+
+export type State = { sort: SortInfo | null }
