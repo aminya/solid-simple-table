a75ee9ac4248eb59eade440db6161f660c5bc971
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-26 05:42:26 -0600

    fix: add parcel plugins to the babel config

--- babel.config.js
+++
@@ -1,6 +0,0 @@
-const plugins = process.env.BABAEL_CJS ? ["@babel/plugin-transform-modules-commonjs"] : []
-
-module.exports = {
-  presets: ["babel-preset-solid", "@babel/preset-typescript"],
-  plugins,
-}
