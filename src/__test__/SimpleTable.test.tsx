c2ccb6dbe7e83adfe27831b076a17dc24c62f529
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-27 08:40:44 -0600

    test: add complex row tests

--- src/__test__/SimpleTable.test.tsx
+++ src/__test__/SimpleTable.test.tsx
@@ -69,8 +69,72 @@ test("renders simple table", () => {
   }
 })
 
-test("renders complex table without crashing", () => {
+test("renders complex table", () => {
   dispose = render(() => <MyComplexTable />, rootElm)
+
+  const children = rootElm.children
+  expect(children.length).toBe(1)
+
+  const table = children[0]
+  expect(table.tagName).toBe("table".toUpperCase())
+  expect(table.classList[0]).toBe("solid-simple-table")
+
+  const tableChildren = table.children
+  expect(tableChildren.length).toBe(2)
+
+  const thead = tableChildren[0]
+  const tbody = tableChildren[1]
+
+  expect(thead.tagName).toBe("thead".toUpperCase())
+  expect(tbody.tagName).toBe("tbody".toUpperCase())
+
+  expect(thead.children.length).toBe(1)
+
+  const tr = thead.firstElementChild
+  expect(tr?.tagName).toBe("tr".toUpperCase())
+
+  // test headers
+  const headers = tr!.children
+
+  expect(headers.length).toBe(MyComplexTableColumns.length)
+
+  for (let iColumn = 0, columnNum = headers.length; iColumn < columnNum; iColumn++) {
+    const header = headers[iColumn]
+    const label = MyComplexTableColumns[iColumn].label
+
+    expect(header.tagName).toBe("th".toUpperCase())
+    if (label === "File") {
+      expect(header.className).toBe("sortable")
+      expect(header.textContent).toBe(`${label}↓`)
+    } else if (label == "Message") {
+      expect(header.className).toBe("undefined")
+      expect(header.textContent).toBe(`${label}`)
+    } else {
+      expect(header.className).toBe("sortable")
+      expect(header.textContent).toBe(`${label}⇅`)
+    }
+  }
+
+  expect(tbody.children.length).toBe(myComplexTableRows.length)
+  const rows = tbody.children
+
+  // test rows
+  for (let iRow = 0, rowNum = rows.length; iRow < rowNum; iRow++) {
+    const row = rows[iRow]
+
+    expect(row.tagName).toBe("tr".toUpperCase())
+
+    // test cells
+    const cells = row.children
+    expect(cells.length).toBe(Object.keys(myComplexTableRows[iRow]).length)
+
+    const myComplexTableRowsValues = Object.values(myComplexTableRows[iRow])
+    for (let iCell = 0, cellNum = cells.length; iCell < cellNum; iCell++) {
+      const cell = cells[iCell]
+      expect(cell.tagName).toBe("td".toUpperCase())
+      expect(cell.textContent).toBe(myComplexTableRowsValues[iCell])
+    }
+  }
 })
 
 afterEach(() => {
