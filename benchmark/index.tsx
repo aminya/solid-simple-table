0e1255f2823b5dd598b4deefcd2bccc5f09979c0
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-12-27 19:40:42 -0600

    chore: add benchmarks

---
+++ benchmark/index.tsx
@@ -0,0 +1,28 @@
+import { render } from "solid-js/web"
+import { SimpleTable } from "../src/SimpleTable"
+
+import { Chance } from 'chance'
+const chance = new Chance()
+
+let rows = new Array(1000)
+
+for (let iRow = 0; iRow < rows.length; iRow++) {
+  rows[iRow] = {
+    "file": chance.sentence({ words: 1 }),
+    "message": chance.sentence({ words: 20 }),
+    "severity": chance.sentence({ words: 1 }),
+  }
+}
+
+export function MySimpleTable() {
+  return <SimpleTable rows={rows} />
+}
+
+// render demo
+
+const ti = window.performance.now()
+
+render(() => <MySimpleTable />, document.getElementById("app")!)
+
+const tf = window.performance.now()
+console.log(`Render time: ${(tf-ti).toFixed()}`)
