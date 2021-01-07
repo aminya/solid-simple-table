c8bf18309f2bc2a406775e88af6123be141648cd
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-01-06 21:42:25 -0600

    chore: move jest.setup file to test folder

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   transform: {
     "^.+\\.[tj]sx?$": "babel-jest",
   },
-  setupFilesAfterEnv: ["./jest.setup.ts"],
+  setupFilesAfterEnv: ["./test/jest.setup.ts"],
   moduleNameMapper: {
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/src/__mocks__/fileMock.js",
